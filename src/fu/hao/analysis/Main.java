d1e8c8402e2e2129a33a11fbf0a0c9b4d70381c8
Hao Fu <haofu@ucdavis.edu>
2018-06-02 16:13:29 -0700

    lint

--- src/fu/hao/analysis/Main.java
+++ src/fu/hao/analysis/Main.java
@@ -18,8 +18,6 @@ import soot.jimple.Stmt;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
 import soot.options.Options;
-import soot.toolkits.graph.ExceptionalUnitGraph;
-import soot.toolkits.graph.UnitGraph;
 import soot.SootMethod;
 
 import org.slf4j.Logger;
@@ -27,7 +25,6 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private static final String TAG = "MAIN";
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     private static SootClass initializeSoot(String className) {
@@ -91,22 +88,16 @@ public class Main {
 
         SootClass tgtClass = initializeSoot("simple-auto-lock-door");
 
-        //Options.v().set_on_the_fly(true);
-        //Options.v().set_whole_program(true);
-        //Options.v().set_allow_phantom_refs(true);
-        //Options.v().setPhaseOption("cg.spark", "on");
-
         // do not merge variables (causes problems with PointsToSets)
         Options.v().setPhaseOption("jb.ulp", "off");
 
 
         // To cope with broken APK files, we convert all classes that are still
         // dangling after resolution into phantoms
-        for (SootClass sc : Scene.v().getClasses())
-            if (sc.resolvingLevel() == SootClass.DANGLING) {
-                sc.setResolvingLevel(SootClass.BODIES);
-                sc.setPhantomClass();
-            }
+        Scene.v().getClasses().stream().filter(sc -> sc.resolvingLevel() == SootClass.DANGLING).forEach(sc -> {
+            sc.setResolvingLevel(SootClass.BODIES);
+            sc.setPhantomClass();
+        });
         // 载入MyClass类
         //SootClass tgtClass = Scene.v().loadClassAndSupport("simple-auto-lock-door");
         //tgtClass.declaresMethodByName("dummyMain");
@@ -121,13 +112,9 @@ public class Main {
 
         Map<Integer, String> callSites = CallGraphResolver.getCallSites(tgtClass);
         System.out.println(callSites);
-        CallGraph callGraph = Scene.v().getCallGraph(); //new CallGraph();
+        CallGraph callGraph = Scene.v().getCallGraph();
 
-
-        // Scene.v().setCallGraph(callGraph);
-
-
-        logger.info(String.valueOf(callGraph.size()));
+        logger.info("cg size: " + String.valueOf(callGraph.size()));
 
         // 找到它的myMethod函数
         //SootMethod method = tgtClass.getMethodByName("checkMotion");
@@ -135,70 +122,6 @@ public class Main {
             if (method.getName().contains("CallSiteArray") || method.getName().matches("run")) {
                 continue;
             }
-
-            System.out.println("---------------------------------------");
-            System.out.println("method: " + method);
-            // 获得它的函数体
-            Body body = method.retrieveActiveBody();
-            // 生成函数的control flow graph
-            UnitGraph cfg = new ExceptionalUnitGraph(body);
-            // 执行我们的分析
-            //TaintForwardAnalysis.TaintForwardVarAnalysis ta = new TaintForwardAnalysis.TaintForwardVarAnalysis(cfg);
-            // iterate over the results
-            for (Unit unit : cfg) {
-                //System.out.println(unit);
-                //List<Local> before = ta.getLiveLocalsBefore(unit);
-                //List<Local> after = ta.getLiveLocalsAfter(unit);
-                UnitPrinter up = new NormalUnitPrinter(body);
-                up.setIndent("");
-
-                //System.out.println("---------------------------------------");
-                unit.toString(up);
-                System.out.println(up.output() + ", " + up.hashCode());
-                Stmt stmt = (Stmt) unit;
-                if (stmt.containsInvokeExpr()) {
-                    stmt.getInvokeExpr().getMethod();
-                }
-
-            /*
-            MethodSummary methodSummary = new MethodSummary(method, callSites);
-            for (String name : methodSummary.getName2stmts().keySet()) {
-                if (name.contains("minutesLater")) {
-                    System.out.println("stmt: " + methodSummary.getName2stmts().get(name));
-                    List<Stmt> slice = slicing(method, methodSummary.getName2stmts().get(name));
-
-                    for (Stmt stmt : slice) {
-                        System.out.println(stmt);
-                    }
-                    methodSummary.interpretation(slice);
-                }
-            } */
-
-            /*
-            if (!before.isEmpty()) {
-                if (unit.toString().contains("sink")) {
-                    System.out.println("found a sink!");
-                }
-            }
-            System.out.print("Taint in: {");
-            sep = "";
-            for (Local l : before) {
-                System.out.print(sep);
-                System.out.print(l.getName() + ": " + l.getType());
-                sep = ", ";
-            }
-            System.out.println("}");
-            System.out.print("Taint out: {");
-            sep = "";
-            for (Local l : after) {
-                System.out.print(sep);
-                System.out.print(l.getName() + ": " + l.getType());
-                sep = ", ";
-            }
-            System.out.println("}");
-            System.out.println("---------------------------------------");*/
-            }
-
             CallGraphResolver.addCallEdges(method, callSites);
         }
 
@@ -209,16 +132,11 @@ public class Main {
                 body.getUnits().insertAfter(newInvoke, old);
                 Edge edge = new Edge(body.getMethod(), newInvoke, newInvoke.getInvokeExpr().getMethod());
                 callGraph.addEdge(edge);
-                for (Unit unit : body.getUnits()) {
-                    Stmt stmt = (Stmt) unit;
-                    logger.info(stmt + ", " + stmt.hashCode());
-                }
                 body.validate();
+                logger.info(old + ", " + newInvoke);
             }
         }
-
-        //InterproceduralCFG<Unit, SootMethod> icfg = new JimpleBasedInterproceduralCFG();
-
+        logger.info("New cg size: " + callGraph.size());
     }
 
 }
